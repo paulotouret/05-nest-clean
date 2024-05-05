7c7a5e42e7bb965d9c82d6405176bf2c947d3873
Paulo Touret <paulotouret16@gmail.com>
2024-05-05 14:02:45 -0300

    fixing some things

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -3,11 +3,12 @@ import { QuestionComment } from '../../enterprise/entities/question-comment'
 
 export abstract class QuestionCommentsRepository {
   abstract findById(id: string): Promise<QuestionComment | null>
+
   abstract findManyByQuestionId(
-    answerId: string,
+    questionId: string,
     params: PaginationParams,
   ): Promise<QuestionComment[]>
 
-  abstract create(answerComment: QuestionComment): Promise<void>
-  abstract delete(answerComment: QuestionComment): Promise<void>
+  abstract create(questionComment: QuestionComment): Promise<void>
+  abstract delete(questionComment: QuestionComment): Promise<void>
 }
